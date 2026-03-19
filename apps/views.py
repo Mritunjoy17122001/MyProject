6568296221e4d241fa390f06e49a21301d3b3350
MRITUNJOY SAHA <mritunjoysaha@MRITUNJOYs-MacBook-Air.local>
2026-03-20 00:21:06 +0530

    Initial commit

--- apps/views.py
+++ apps/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to MyProject!")
+
+
+def home2(request):
+    return HttpResponse("Welcome to MyProject!")
